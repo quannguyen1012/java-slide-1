4ea8756497d89b76409709cd49b2a65db9cc795f
user <quans1471@gmail.com>
2022-09-06 14:27:04 +0700

    file-bai-tap

---
+++ KBBien.java
@@ -0,0 +1,13 @@
+public class KBBien {
+    public static void main(String[] args) {
+        int a = 10;
+        float b = 20.5f;
+        double c = 20.5;
+        boolean d = true;
+        char e = 'a';
+        String f = "Hà Nội";
+
+        System.out.println(a +  "\n" + b + "\n" + c + "\n" + d + "\n" + e + "\n" + f);
+
+    }
+}
